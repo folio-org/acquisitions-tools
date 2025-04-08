53967f8e7d33ed019ed5bcf6addc4e93bd84c979
Azizbek Khushvakov <azizbek_khushvakov@epam.com>
2025-04-08 17:55:56 +0500

    Implement location update API by holdings

--- src/main/java/org/folio/acquisitions_tools/service/DatabaseService.java
+++ src/main/java/org/folio/acquisitions_tools/service/DatabaseService.java
@@ -12,7 +12,7 @@ public class DatabaseService {
   @Autowired
   private JdbcTemplate jdbcTemplate;
 
-  public void updatePoLineLocations(String tenant, Map<String, String> tenantsGroupedByLocation) {
+  public void updatePoLineLocationsByLocation(String tenant, Map<String, String> tenantsGroupedByLocation) {
     for (Map.Entry<String, String> location : tenantsGroupedByLocation.entrySet()) {
       String locationId = location.getKey();
       String locationTenantId = location.getValue();
@@ -44,7 +44,39 @@ public class DatabaseService {
     }
   }
 
-  public void updatePieces(String tenant, Map<String, String> tenantsGroupedByLocation) {
+  public void updatePoLineLocationsByHolding(String tenant, Map<String, String> tenantsGroupedByHolding) {
+    for (Map.Entry<String, String> holding : tenantsGroupedByHolding.entrySet()) {
+      String holdingId = holding.getKey();
+      String holdingTenantId = holding.getValue();
+
+      String sql = """
+         UPDATE %s_mod_orders_storage.po_line
+         SET jsonb = jsonb_set(
+           jsonb,
+           '{locations}',
+           (
+             SELECT jsonb_agg(
+               CASE
+                 WHEN location ? 'tenantId' THEN location
+                 ELSE jsonb_set(location, '{tenantId}', '"%s"'::jsonb)
+               END
+             )
+             FROM jsonb_array_elements(jsonb->'locations') AS location
+           )
+         )
+         WHERE\s
+             jsonb->'locations' @> '[{"holdingId": "%s"}]'
+             AND NOT (
+                 SELECT bool_and(elem ? 'tenantId')
+                 FROM jsonb_array_elements(jsonb->'locations') AS elem
+             )
+        \s""";
+
+      jdbcTemplate.update(String.format(sql, tenant, holdingTenantId, holdingId));
+    }
+  }
+
+  public void updatePiecesByLocation(String tenant, Map<String, String> tenantsGroupedByLocation) {
     for (Map.Entry<String, String> location : tenantsGroupedByLocation.entrySet()) {
       String locationId = location.getKey();
       String locationTenantId = location.getValue();
@@ -62,6 +94,27 @@ public class DatabaseService {
         \s""";
 
       jdbcTemplate.update(String.format(sql, tenant, locationTenantId, locationId));
+    }
+  }
+
+  public void updatePiecesByHolding(String tenant, Map<String, String> tenantsGroupedByHolding) {
+    for (Map.Entry<String, String> holding : tenantsGroupedByHolding.entrySet()) {
+      String holdingId = holding.getKey();
+      String holdingTenantId = holding.getValue();
+
+      String sql = """
+         UPDATE %s_mod_orders_storage.pieces
+         SET jsonb = jsonb_set(
+             jsonb,
+             '{receivingTenantId}',
+             '"%s"'::jsonb
+         )
+         WHERE\s
+             jsonb->>'holdingId' = '%s'
+             AND NOT (jsonb ? 'receivingTenantId')
+        \s""";
+
+      jdbcTemplate.update(String.format(sql, tenant, holdingTenantId, holdingId));
     }
   }
 } 
--- src/main/java/org/folio/acquisitions_tools/service/LocationService.java
+++ src/main/java/org/folio/acquisitions_tools/service/LocationService.java
@@ -7,6 +7,7 @@ import java.util.stream.StreamSupport;
 
 import com.fasterxml.jackson.databind.JsonNode;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
@@ -15,6 +16,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+@Slf4j
 @Service
 public class LocationService {
   private final RestTemplate restTemplate = new RestTemplate();
@@ -31,13 +33,34 @@ public class LocationService {
   @Value("${folio.api.locations.endpoint}")
   private String locationsEndpoint;
 
+  @Value("${folio.api.holdings.endpoint}")
+  private String holdingsEndpoint;
+
   @Value("${folio.api.tenant}")
   private String tenant;
 
   public Map<String, String> getLocationIdsAndTenants() {
     HttpEntity<String> request = new HttpEntity<>(loginService.getHeaders());
     ResponseEntity<JsonNode> response = restTemplate.exchange(
-      okapiUrl + locationsEndpoint + "?tenantId=" + tenant,
+      okapiUrl + locationsEndpoint,
+      HttpMethod.GET,
+      request,
+      JsonNode.class
+    );
+
+    JsonNode locations = Objects.requireNonNull(response.getBody()).get("locations");
+
+    return StreamSupport.stream(locations.spliterator(), false)
+      .collect(Collectors.toMap(
+        loc -> loc.get("id").asText(),
+        loc -> loc.get("tenantId").asText()
+      ));
+  }
+
+  public Map<String, String> getHoldingsAndTenants() {
+    HttpEntity<String> request = new HttpEntity<>(loginService.getHeaders());
+    ResponseEntity<JsonNode> response = restTemplate.exchange(
+      okapiUrl + holdingsEndpoint,
       HttpMethod.GET,
       request,
       JsonNode.class
@@ -54,11 +77,21 @@ public class LocationService {
 
   public void updatePoLineLocations() {
     Map<String, String> tenantsGroupedByLocation = getLocationIdsAndTenants();
-    databaseService.updatePoLineLocations(tenant, tenantsGroupedByLocation);
+    log.info("updatePoLineLocations:: Updating poLine locations by locationId");
+    databaseService.updatePoLineLocationsByLocation(tenant, tenantsGroupedByLocation);
+
+    Map<String, String> tenantsGroupedByHolding = getHoldingsAndTenants();
+    log.info("updatePoLineLocations:: Updating poLine locations by holdingId");
+    databaseService.updatePoLineLocationsByHolding(tenant, tenantsGroupedByHolding);
   }
 
   public void updatePieces() {
     Map<String, String> tenantsGroupedByLocation = getLocationIdsAndTenants();
-    databaseService.updatePieces(tenant, tenantsGroupedByLocation);
+    log.info("updatePieces:: Updating pieces locations by locationId");
+    databaseService.updatePiecesByLocation(tenant, tenantsGroupedByLocation);
+
+    Map<String, String> tenantsGroupedByHolding = getHoldingsAndTenants();
+    log.info("updatePieces:: Updating pieces locations by holdingId");
+    databaseService.updatePiecesByHolding(tenant, tenantsGroupedByHolding);
   }
 } 
