7844b679f6595eff30e5cf1806d5055e11e60146
Azizbek Khushvakov <azizbek_khushvakov@epam.com>
2025-04-09 18:11:09 +0500

    add timeout to RestTemplate

--- src/main/java/org/folio/acquisitions_tools/config/RestTemplateConfig.java
+++ src/main/java/org/folio/acquisitions_tools/config/RestTemplateConfig.java
@@ -2,12 +2,19 @@ package org.folio.acquisitions_tools.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class RestTemplateConfig {
+
   @Bean
   public RestTemplate restTemplate() {
-    return new RestTemplate();
+    RestTemplate restTemplate = new RestTemplate();
+    restTemplate.setRequestFactory(new SimpleClientHttpRequestFactory());
+    SimpleClientHttpRequestFactory rf = (SimpleClientHttpRequestFactory) restTemplate.getRequestFactory();
+    rf.setReadTimeout(5000);
+    rf.setConnectTimeout(5000);
+    return restTemplate;
   }
 }
